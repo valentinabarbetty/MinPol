65b39a41b2f4209af62126ba23490bd4ac047af2
BrianGomezM <brayan.julio.gomez@correounivalle.edu.co>
2024-10-23 14:53:48 -0500

    act

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+ imports: [MatTooltipModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  tooltipContent: string = "Integrantes:<br> Brayan Gomez Muñoz<br>    Valentina Barbetty Arango<br>    Jheison Estiben Gomez Muñoz";
+    
+  
 
 }
