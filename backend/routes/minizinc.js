9b3acf1f5c1c62779289c3d02eb79a918f0c34f7
BrianGomezM <brayan.julio.gomez@correounivalle.edu.co>
2024-11-02 20:06:55 -0500

    act

--- backend/routes/minizinc.js
+++ backend/routes/minizinc.js
@@ -10,25 +10,23 @@ router.post('/minPol', (req, res) => {
 
     // Crear contenido para el archivo de datos MiniZinc
     const dataContent = `
-        numPersonas = ${numPersonas};
-        numOpiniones = ${numOpiniones};
-        distribucionOpiniones = [${distribucionOpiniones.join(', ')}];
-        valoresOpiniones = [${valoresOpiniones.join(', ')}];
-        costosExtras = [${costosExtras.join(', ')}];
-        costosDesplazamiento = array2d(1..${numOpiniones}, 1..${numOpiniones}, [${costosDesplazamiento.flat().join(', ')}]);
-        costoTotalMax = ${costoTotalMax};
-        maxMovimientos = ${maxMovimientos};
+        n = ${numPersonas};
+        m = ${numOpiniones};
+        p = [${distribucionOpiniones.join(', ')}];
+        v = [${valoresOpiniones.join(', ')}];
+        ce = [${costosExtras.join(', ')}];
+        c = array2d(1..${numOpiniones}, 1..${numOpiniones}, [${costosDesplazamiento.flat().join(', ')}]);
+        ct = ${costoTotalMax};
+        maxM = ${maxMovimientos};
     `;
-
     // Escribir archivo data.dzn
     fs.writeFileSync('data.dzn', dataContent);
 
     // Ruta al archivo .mzn de MiniZinc
     const minpolPath = path.join(__dirname, '..', 'minizinc', 'minpol.mzn');
     const command = `minizinc --solver Gecode "${minpolPath}" data.dzn`;
-
     exec(command, (error, stdout, stderr) => {
-        fs.unlinkSync('data.dzn');
+        fs.unlinkSync('data.dzn'); // Eliminar el archivo de datos una vez ejecutado
         if (error) {
             console.error(`Error al ejecutar MiniZinc: ${error.message}`);
             return res.status(500).send('Error al ejecutar MiniZinc');
@@ -37,10 +35,22 @@ router.post('/minPol', (req, res) => {
             console.error(`stderr: ${stderr}`);
             return res.status(500).send('Error en la ejecución de MiniZinc');
         }
-
-        const resultadoLimpiado = stdout.split('\n')[0].trim();
-        res.send({ resultado: resultadoLimpiado });
-    });
+    
+        // Extraer el valor de la polarización mínima y los movimientos usando expresiones regulares
+        const matchPolarizacion = stdout.match(/Polarizacion minima = (.*)/);
+        const matchMovimientos = stdout.match(/Movimientos entre opiniones:/);
+    
+        if (matchPolarizacion && matchMovimientos) {
+            const resultadoPolarizacion = matchPolarizacion[1].trim();
+            const movimientos = stdout.split(/Movimientos entre opiniones:/)[1].trim();
+            res.send({ 
+                resultado: resultadoPolarizacion,
+                movimientos: movimientos
+            });
+        } else {
+            res.status(500).send('No se pudo encontrar el resultado en la salida de MiniZinc');
+        }
+    });    
 });
 
 module.exports = router;
