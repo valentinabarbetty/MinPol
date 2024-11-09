f1f687e45dae459385f692c6169da307deb15dee
BrianGomezM <brayan.julio.gomez@correounivalle.edu.co>
2024-11-09 18:12:11 -0500

    docker-file

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,9 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:4200', // Permite un origen dinámico
-    optionsSuccessStatus: 200
-  };
+  origin: '*',
+  optionsSuccessStatus: 200
+};
+
   
 module.exports = cors(corsOptions);
